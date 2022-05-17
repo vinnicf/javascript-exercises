f25ced8b09e8f3c7ee53834c6f6b790ebc02b391
Vini Costa <vinnicf@gmail.com>
2022-05-17 11:09:42 -0300

    Complete Exercise 01-03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
 
-};
+if (num >= 0) {
+
+
+  let myst = '';
+    for (let i = 0; i < num; i++) {
+      myst = myst + string;
+    }
+    return myst;
+  }
+ else {
+   return 'ERROR';
+ }
+      };
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(text) {
 
+//Transform string into an array with the all the characters using a loop
+let splittext = []
+for (let i = 0; i < text.length; i++) {
+    splittext.push(text[i]);
+
+}
+const reversearray = splittext.reverse();
+const jointext = reversearray.join('');
+
+//Transform the array back into a newstring by looping inversely
+
+return jointext;
 };
 
 // Do not edit below this line
